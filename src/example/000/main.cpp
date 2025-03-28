77369ea97b09237759dba577878958a26d81dd09
chaojun.xiao <chaojun.xiao@mthreads.com>
2025-03-28 18:03:42 +0800

    add: add example 000
    Signed-off-by: chaojun.xiao <chaojun.xiao@mthreads.com>

---
+++ src/example/000/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello All, welcome to study CMake 4.0 with eglinux." << std::endl;
+    std::cout << "Hello cpp " << __cplusplus << std::endl;
+}
